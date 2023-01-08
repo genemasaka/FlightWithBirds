2a9fc6e4e8ec35c9dad0faad9fa8aa1b4036e6f5
Eugene Masaka <eugenemasaka777@gmail.com>
2023-01-08 19:54:13 +0300

    NavBar

---
+++ src/navBar.js
@@ -0,0 +1,18 @@
+import Button from 'react-bootstrap/Button';
+
+export function NavBar() {
+    return (
+        <>
+        <nav class="navbar navbar-light bg-light">
+        <div class="container-fluid">
+        <span class="navbar-brand mb-0 h1">flight with birds</span>
+        <a class="navbar-brand" href="#">Shop</a>
+        <a class="navbar-brand" href="#">Lookbooks</a>
+        <a class="navbar-brand" href="#">Blog</a>
+        <Button variant="primary">Primary</Button>{' '}
+        </div>
+        </nav>
+        
+        </>
+    );
+}       
