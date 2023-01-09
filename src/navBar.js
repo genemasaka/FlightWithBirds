18b46eda3fe81a6067666874756fa75fb96af099
Eugene Masaka <eugenemasaka777@gmail.com>
2023-01-09 14:54:55 +0300

    styled navbar

--- src/navBar.js
+++ src/navBar.js
@@ -1,15 +1,19 @@
 import Button from 'react-bootstrap/Button';
 
+import './App.css';
 export function NavBar() {
     return (
         <>
-        <nav class="navbar navbar-light bg-light">
+        <nav class="navbar fixed-top navbar-light bg-light">
         <div class="container-fluid">
         <span class="navbar-brand mb-0 h1">flight with birds</span>
-        <a class="navbar-brand" href="#">Shop</a>
-        <a class="navbar-brand" href="#">Lookbooks</a>
-        <a class="navbar-brand" href="#">Blog</a>
-        <Button variant="dark">Connect Wallet</Button>{' '}
+        <a class="navbar-brand" href="#" ><em>Shop</em></a>
+        <a class="navbar-brand" href="#"><em>Lookbooks</em></a>
+        <a class="navbar-brand" href="#"><em>Blog</em></a>
+        <Button variant="dark" class="btn"><em>Connect Wallet</em></Button>{' '}
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+        </button>
         </div>
         </nav>
         
